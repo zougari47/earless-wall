8ea3a5a4a96b5ce65dc735a97c153c682a034320
Ahmed Zougari <zougarii.ahmed@gmail.com>
2026-01-11 07:24:30 +0100

    initial commit

--- components/Button.tsx
+++ components/Button.tsx
@@ -19,6 +19,6 @@ export const Button = forwardRef<View, ButtonProps>(({ title, ...touchableProps 
 Button.displayName = 'Button';
 
 const styles = {
-  button: 'items-center bg-indigo-500 rounded-[28px] shadow-md p-4',
+  button: 'items-center bg-blue-500 rounded-[28px] shadow-md p-4',
   buttonText: 'text-white text-lg font-semibold text-center',
 };
--- metro.config.js
+++ metro.config.js
@@ -7,4 +7,4 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = withNativeWind(config, { input: './global.css', inlineRem: 16 });
